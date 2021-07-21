88f1d1c9dedf498e3ff589b2e1715df7b0954051
bdurham227 <bdurham227@gmail.com>
2021-07-21 15:16:36 -0400

    built out models

---
+++ models/Comment.js
@@ -0,0 +1,49 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    comment: {
+        type: DataTypes.STRING,
+        validate: {
+            len: [3]
+        }
+    },
+    date_created: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+    },
+    date_updated: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+    },
+    post_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'blog',
+            key: 'id',
+        },
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'user',
+            key: 'id',
+        },
+    },
+}, {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment',
+});
+
+module.exports = Comment;
